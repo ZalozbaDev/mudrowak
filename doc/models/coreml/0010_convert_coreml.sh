b1b48a5a04a657dce0402e2f0477df65f3cf27b1
Daniel Zoba <danielzoba@Mac.fritz.box>
2025-12-03 08:49:33 +0100

    less hard-coding for the coreml converter

--- doc/models/coreml/0010_convert_coreml.sh
+++ doc/models/coreml/0010_convert_coreml.sh
@@ -2,25 +2,47 @@
 
 source pythonenv/bin/activate
 
-# remove leftovers from previous runs
+for i in Korla/whisper_large_v3_turbo_hsb Korla/whisper_large_v3_turbo_hsb-0 ; do
+    
+    # remove leftovers from previous runs
+    pushd whisper.cpp/
+    git reset --hard
+    git clean -fdx
+    popd
 
-cd whisper.cpp/
-git reset --hard
-git clean -fdx
-cd ..
+    echo
+    echo "================================================"
+    echo "Checking model $i for conversion"
+    echo "================================================"
+    echo
+    CACHEDIR=$(echo $i | tr '/' '_')
 
-## hard-coded to convert one model only
+    if [ -e ~/cache/${CACHEDIR} ] && [ -e ~/whisper_models/${i} ]; then
 
-cp ~/whisper_models/Korla/whisper_large_v3_turbo_hsb/ggml-model.bin whisper.cpp/models/ggml-large-v3-turbo.bin
+        echo "Running coreml conversion."
 
-cd whisper.cpp
+        # this is hard-coded until we need to distinguish the model type/arch
+        MODEL_TYPE_ARCH=large-v3-turbo
 
-./models/generate-coreml-model.sh -h5 large-v3-turbo ~/cache/Korla_whisper_large_v3_turbo_hsb/
+        cp ~/whisper_models/$i/ggml-model.bin whisper.cpp/models/ggml-${MODEL_TYPE_ARCH}.bin
 
-rm -rf ~/whisper_models/Korla/whisper_large_v3_turbo_hsb/coreml/
-mkdir -p ~/whisper_models/Korla/whisper_large_v3_turbo_hsb/coreml/
-cp models/ggml-large-v3-turbo.bin                 ~/whisper_models/Korla/whisper_large_v3_turbo_hsb/coreml/
-cp -r models/ggml-large-v3-turbo-encoder.mlmodelc ~/whisper_models/Korla/whisper_large_v3_turbo_hsb/coreml/
+        pushd whisper.cpp
 
-cd ..
+        ./models/generate-coreml-model.sh -h5 ${MODEL_TYPE_ARCH} ~/cache/$CACHEDIR/
+
+        rm -rf ~/whisper_models/$i/coreml/
+        mkdir -p ~/whisper_models/$i/coreml/
+        cp models/ggml-${MODEL_TYPE_ARCH}.bin                     ~/whisper_models/$i/coreml/
+        cp -r models/ggml-${MODEL_TYPE_ARCH}-encoder.mlmodelc ~/whisper_models/$i/coreml/
+
+        popd
+
+    else
+
+        echo "Skipping $i - did not find cache at ~/cache/${CACHEDIR} and output directory at ~/whisper_models/${i}! You might need to regenerate it."
+        echo
+
+    fi
+
+done
 

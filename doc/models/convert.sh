62ffa6ec83fc5c2719ffaf47fe5291e66dc1ca3f
Daniel Zoba <d.zoba@zalozba.de>
2026-06-10 13:54:51 +0200

    add model

--- doc/models/convert.sh
+++ doc/models/convert.sh
@@ -402,6 +402,36 @@ case $MODEL in
 		python3 ./models/convert-h5-to-ggml.py /cache/Korla_whisper_test/ /cache/openai_whisper/ /output/Korla/whisper_test/
 		;;
 	
+	Korla/whisper-large-v3-turbo-hsb-v1)
+		if [ ! -e /cache/Korla_whisper_large_v3_turbo_hsb-v1 ]; then
+			git clone https://huggingface.co/Korla/whisper-large-v3-turbo-hsb-v1 /cache/Korla_whisper_large_v3_turbo_hsb-v1
+		fi
+		if [ ! -e /cache/openai_whisper ]; then
+			git clone https://github.com/openai/whisper                /cache/openai_whisper
+		fi
+		pushd /cache/openai_whisper && git checkout $OPENAI_WHISPER_TAG_LATEST && popd
+		if [ ! -e /cache/openai_whisper_large_v3_turbo ]; then
+			GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/openai/whisper-large-v3-turbo      /cache/openai_whisper_large_v3_turbo
+		fi
+
+		# possibly missing files
+		if [ ! -e /cache/Korla_whisper_large_v3_turbo_hsb-v1/vocab.json ]; then
+			cp /cache/openai_whisper_large_v3_turbo/vocab.json        /cache/Korla_whisper_large_v3_turbo_hsb-v1/
+		fi
+		if [ ! -e /cache/Korla_whisper_large_v3_turbo_hsb-v1/added_tokens.json ]; then
+			cp /cache/openai_whisper_large_v3_turbo/added_tokens.json /cache/Korla_whisper_large_v3_turbo_hsb-v1/
+		fi
+		if [ ! -e /cache/Korla_whisper_large_v3_turbo_hsb-v1/generation_config.json ]; then
+			cp /cache/openai_whisper_large_v3_turbo/generation_config.json        /cache/Korla_whisper_large_v3_turbo_hsb-v1/
+		fi
+		
+        ## GGML ##
+
+		mkdir -p /output/Korla/whisper_large_v3_turbo_hsb-v1
+		cd $WHISPER_V1_7_4
+		python3 ./models/convert-h5-to-ggml.py /cache/Korla_whisper_large_v3_turbo_hsb-v1/ /cache/openai_whisper/ /output/Korla/whisper_large_v3_turbo_hsb-v1/
+		;;
+	
 	*)
 		echo "Model $MODEL unknown!"
 		;;
